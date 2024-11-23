bd1517e70ca95ec38f64e6996d804916bcb96c12
Fitrah Eka Ardiansyah <ardiansyahfitrah17@gmail.com>
2024-11-23 13:41:31 +0700

    setup page admin untuk manage account user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'gender', 'is_superuser')
+    list_filter = ('gender', 'status',)
+    search_fields = ('username', 'email')
+    list_per_page = 10
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
