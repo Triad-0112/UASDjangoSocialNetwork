c9a7b1991a256377e92954819fb9a4608056f358
Fitrah Eka Ardiansyah <ardiansyahfitrah17@gmail.com>
2024-11-23 13:57:36 +0700

    update posisi kolom di manage post

--- newsfeed/admin.py
+++ newsfeed/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('user', 'body', 'image',)
+    list_display = ('body', 'image', 'user',)
     list_filter = ('user__username',)
     search_fields = ('user__username', 'body')
     list_per_page = 10
