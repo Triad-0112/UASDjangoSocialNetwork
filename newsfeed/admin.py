089d9324da0a919d7ff0a5fcf14b821e212915c1
Fitrah Eka Ardiansyah <ardiansyahfitrah17@gmail.com>
2024-11-23 13:56:27 +0700

    penambahan menu manage post di admin

--- newsfeed/admin.py
+++ newsfeed/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'body', 'image',)
+    list_filter = ('user__username',)
+    search_fields = ('user__username', 'body')
+    list_per_page = 10
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
